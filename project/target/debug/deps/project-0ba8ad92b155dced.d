619870dab490bde7e36aa59b711b88c1aa2b850e
skye-zhanyingjiang <tom.zhanyingjiang@gmail.com>
2024-08-20 15:13:47 +0800

    add code

---
+++ project/target/debug/deps/project-0ba8ad92b155dced.d
@@ -0,0 +1,5 @@
+/Users/degame/Documents/rust_code/rust/project/target/debug/deps/project-0ba8ad92b155dced: src/main.rs
+
+/Users/degame/Documents/rust_code/rust/project/target/debug/deps/project-0ba8ad92b155dced.d: src/main.rs
+
+src/main.rs:
